01fa295501d54019dadeb7b7d527dbf193f0d075
ju oliveira <juliaoliver.veleiros@gmail.com>
2024-09-24 16:38:59 -0300

    update contato rodapé

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const toggleTheme = document.getElementById("toggleTheme");
 
 const roothtml = document.documentElement;
 
+const accordionHeaders = document.querySelectorAll(".accordion-header")
+
 //função para alternar o tema
 function changeTheme () {
     const correntTheme = roothtml.getAttribute("data-theme");
@@ -15,3 +17,12 @@ function changeTheme () {
 
 toggleTheme.addEventListener("click", changeTheme);
 //fim da função mudar o tema
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
